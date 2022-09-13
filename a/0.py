e994bb7ddf98a54d8b76ca4fde3397d6aa4b6639
alparslan bzk <70051862+alparslanbzk@users.noreply.github.com>
2022-09-13 23:13:27 +0300

    Create 0.py

---
+++ a/0.py
@@ -0,0 +1,24 @@
+# 0 - youtube links collect
+from da import Channel 
+from da import YouTube
+
+ 
+ 
+def main(): 
+    
+    c = Channel("https://www.youtube.com/c/whitehouse45/videos") 
+    number = 0
+    for url in c.video_urls: 
+        yt = YouTube('https://www.youtube.com/watch?v='+url)
+        y = str(yt.captions) 
+        #hello = yt.captions
+        index = y.find('<Caption lang="English" code="en">')
+        if index == 7:
+            print(url)
+       
+        
+        
+ 
+ 
+if __name__ == '__main__': 
+    main() 

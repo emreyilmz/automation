8fed733759aa4753259a439d5e9367db52d23e40
alparslan bzk <70051862+alparslanbzk@users.noreply.github.com>
2022-09-13 23:14:45 +0300

    Create 6.py

---
+++ a/6.py
@@ -0,0 +1,17 @@
+# 6 - timestamps extract
+
+import re
+import os
+#print("\nWe are listing out only the directories in current directory -")
+directories_in_curdir = list(filter(os.path.isdir, os.listdir(os.curdir)))
+#print(type(directories_in_curdir))
+
+pattern = r'(\d{2}:\d{2}:\d{2},d{3}$)|(\d{2}:\d{2}:\d{2})'
+for a in directories_in_curdir:
+    with open("./"+a+"/subtitle.vtt", 'r', encoding='utf-8') as f:
+        text = f.readlines()
+        for i in range(len(text)):
+            if re.match(pattern, text[i]):
+                print(text[i])
+                fout = open("./"+a+"/timestamps.vtt", "a")
+                fout.write(text[i])
